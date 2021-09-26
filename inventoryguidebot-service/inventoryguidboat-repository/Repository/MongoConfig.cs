f7c173564266c00560e5e05c10fffa9298474ee0
Basayya <basayya.kulkarni@centurylink.com>
2021-09-26 17:56:39 +0530

    updated the mongodb connection string

--- inventoryguidebot-service/inventoryguidboat-repository/Repository/MongoConfig.cs
+++ inventoryguidebot-service/inventoryguidboat-repository/Repository/MongoConfig.cs
@@ -11,7 +11,9 @@ namespace inventoryguidboat_repository.Repository
     {
         public MongoClient GetMongoClient()
         {
-            var client = new MongoClient("mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false");
+            //mongodb + srv://admin:admin@cluster0.receh.mongodb.net/test?authSource=admin&replicaSet=atlas-g7tu3n-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true
+            //var client = new MongoClient("mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false");
+            var client = new MongoClient("mongodb+srv://admin:admin@cluster0.receh.mongodb.net/inventory?retryWrites=true&w=majority");
             return client;
         }
     }
